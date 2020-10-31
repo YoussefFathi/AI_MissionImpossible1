3df3a3e73312d63c1f2e4c5476796737d70b5c48
YoussefFathi <youssef.fathy1164@gmail.com>
2020-11-01 01:37:00 +0200

    backbone

---
+++ AI_Project1/Project1/src/Mission_Impossible.java
@@ -0,0 +1,18 @@
+public class Mission_Impossible extends  Generic_Problem{
+	
+	public Mission_Impossible() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean goalTest(String currentState) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
