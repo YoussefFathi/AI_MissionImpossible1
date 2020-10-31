8c7d4ff82e1a477b9528b5b00228a694f9952dc9
YoussefFathi <youssef.fathy1164@gmail.com>
2020-11-01 01:31:48 +0200

    Backbone1

---
+++ AI Project 1/Project1/src/Generic_Problem.java
@@ -0,0 +1,13 @@
+
+public abstract class Generic_Problem {
+	
+	public String initialState;
+	public Operators operators;
+	public String stateSpace;
+	public String pathCost;
+	public abstract boolean goalTest(String currentState);
+	
+	
+	
+
+}
---
+++ AI Project 1/Project1/src/Generic_Search.java
@@ -0,0 +1,13 @@
+
+public class Generic_Search {
+	
+	public static void general_search(String problem,) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
